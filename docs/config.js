6e207c8bf5277616a3b82b688e66fcb4d077175d
urah_meshiya <ponzu946@gmail.com>
2026-08-25 16:41:59 +0900

    edit config

--- docs/config.js
+++ docs/config.js
@@ -1,10 +1,10 @@
 // ここを自分の環境に合わせて書き換えてください。
 window.TWICAS_GHOST_CONFIG = {
   // Cloudflare Workerをデプロイした後のURL（wss://から始まる）
-  workerUrl: "wss://twicas-ghost-worker.YOUR-SUBDOMAIN.workers.dev/ws",
+  workerUrl: "wss://twicas-ghost-worker.ponzu946.workers.dev/ws",
 
   // 監視したいツイキャス配信者のscreen_id（URLの twitcasting.tv/xxxxx の xxxxx 部分）
-  screenId: "your_screen_id",
+  screenId: "urah_meshiya",
 
   // 吹き出しを表示しておく時間(ミリ秒)
   bubbleDurationMs: 4500,
